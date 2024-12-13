d7cfb19ff02b04d44361e6f16ba816e8f880c572
Andrey <a.coblov@yandex.ru>
2024-12-14 00:18:39 +0400

    add maxlegth input

--- resources/views/livewire/checkout-form.blade.php
+++ resources/views/livewire/checkout-form.blade.php
@@ -28,21 +28,24 @@
                     <input type="text"
                            wire:model="tickets.{{ $index }}.name"
                            class="w-full px-3 py-2 border rounded"
-                           placeholder="Введите имя">
+                           placeholder="Введите имя"
+                           maxlength="50">
                 </div>
                 <div class="mb-4">
                     <label class="block text-sm font-medium text-gray-700">Email</label>
                     <input type="email"
                            wire:model="tickets.{{ $index }}.email"
                            class="w-full px-3 py-2 border rounded"
-                           placeholder="Введите email">
+                           placeholder="Введите email"
+                           maxlength="50">
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700">Телефон</label>
                     <input type="text"
                            wire:model="tickets.{{ $index }}.phone"
                            class="w-full px-3 py-2 border rounded"
-                           placeholder="Введите телефон">
+                           placeholder="Введите телефон"
+                           maxlength="11">
                 </div>
             </div>
         @endforeach
