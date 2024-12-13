8191d36e8a118d17f548779a33bd6008ac646cce
Andrey <a.coblov@yandex.ru>
2024-12-13 21:52:55 +0400

    fix image

--- resources/views/livewire/ticket-modal.blade.php
+++ resources/views/livewire/ticket-modal.blade.php
@@ -1,16 +1,16 @@
 <div>
     @if ($isOpen)
         <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div class="bg-white rounded-lg shadow-lg w-[70%] h-[70%] relative flex flex-col">
+            <div class="bg-white rounded-lg shadow-lg w-[70%] h-[90%] relative flex flex-col">
                 <div class="modal-header flex justify-between items-center p-4 border-b">
                     <h5 class="text-xl font-bold">{{ $title }}</h5>
                     <button type="button" wire:click="closeModal" class="text-gray-500 hover:text-gray-700 text-2xl font-bold">&times;</button>
                 </div>
 
-                <div class="modal-body p-4 overflow-auto flex-grow">
+                <div class="flex flex-col modal-body p-4 overflow-auto flex-grow">
                     <p class="text-lg">{{ $description }}</p>
                     @if ($image)
-                        <img src="{{ $image }}" alt="Image" class="w-full h-auto rounded mb-4 mt-4">
+                        <img src="{{ $image }}" alt="Image" class="w-full h-[60%] rounded mb-4 mt-4 object-cover">
                     @endif
 
                     @if($type === 'ticket')
