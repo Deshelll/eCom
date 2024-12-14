3a2111cbd400d6154a95ddff9d7508452d5de246
Georgy <gosha.balashov.1002@gmail.com>
2024-12-14 07:42:23 +0400

    Last one

--- resources/views/livewire/card-create.blade.php
+++ resources/views/livewire/card-create.blade.php
@@ -1,7 +1,7 @@
 <div class="card border-0 rounded-lg p-4"
      wire:click="$dispatch('openModal', { id: {{ $card->id }}, type: 'ticket' })">
     <img src="{{ $card['image'] }}" alt="{{ $card['title'] }}" class="w-full h-40 object-cover rounded-t-lg">
-    <h3 class="text-lg font-bold mt-2">{{ $card['title'] }}</h3>
-    <p class="text-sm text-gray-600">{{ $card['description'] }}</p>
-    <p class="text-md font-semibold text-teal-500 mt-1">{{ $card['price'] }} руб.</p>
+    <h3 class="text-xl font-semibold text-gray-800 mt-2">{{ $card['title'] }}</h3>
+    <p class="text-sm text-gray-600 mt-1">{{ $card['description'] }}</p>
+    <p class="text-lg font-bold text-teal-600 mt-2">{{ $card['price'] }} ₽</p>
 </div>
